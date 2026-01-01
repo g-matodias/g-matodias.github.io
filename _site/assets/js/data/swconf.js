d25262abb37eac4a76f903e4ea46b83e94940f1a
g-matodias <rdmt12@protonmail.com>
2026-01-01 13:11:23 -0800

    Update favicon and social parameters

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767299680',resources: [
+    cacheName: 'chirpy-1767301776',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
