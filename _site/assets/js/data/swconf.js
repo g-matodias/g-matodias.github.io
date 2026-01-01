81f07456634545f0166d6b1e894af2a4c3a05843
g-matodias <rdmt12@protonmail.com>
2026-01-01 12:36:36 -0800

    #

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767298454',resources: [
+    cacheName: 'chirpy-1767299680',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
